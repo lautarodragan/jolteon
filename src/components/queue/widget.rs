ee57025a57e6d753de4c40617d5b2ed0948a6275
taro <taro@taro.codes>
2024-11-05 20:32:21 -0300

    feat(queue widget): margin

--- src/components/queue/widget.rs
+++ src/components/queue/widget.rs
@@ -1,5 +1,5 @@
 use ratatui::buffer::Buffer;
-use ratatui::layout::Rect;
+use ratatui::layout::{Constraint, Layout, Rect};
 use ratatui::prelude::{Modifier, Style};
 use ratatui::widgets::{List, ListState, StatefulWidget, WidgetRef};
 
@@ -9,6 +9,12 @@ use super::queue::Queue;
 
 impl WidgetRef for Queue {
     fn render_ref(&self, area: Rect, buf: &mut Buffer) {
+        let [area] = Layout::horizontal([
+            Constraint::Percentage(100),
+        ])
+            .horizontal_margin(2)
+            .areas(area);
+
         let queue_items: Vec<String> = self.songs().iter().map(ui::song_to_string).collect();
 
         let queue_list = List::new(queue_items)
