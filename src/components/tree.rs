799999ecf6e6258cf569b5611b9b7e402ad9556c
taro <taro@taro.codes>
2025-01-30 19:19:07 -0300

    refactor(Tree): tree_node_path.rs

--- src/components/tree.rs
+++ src/components/tree.rs
@@ -1,5 +1,7 @@
 mod component;
 mod keyboard_handler;
 mod widget;
+mod tree_node_path;
 
 pub use component::{Tree, TreeNode};
+pub use tree_node_path::{TreeNodePath};
--- src/components/tree/component.rs
+++ src/components/tree/component.rs
@@ -13,73 +13,7 @@ use crate::{
     structs::Direction,
 };
 
-#[derive(Clone, Debug, Eq, PartialEq, PartialOrd)]
-pub struct TreeNodePath(Vec<usize>);
-
-impl TreeNodePath {
-    fn new() -> Self {
-        Self(vec![])
-    }
-
-    fn parent(&self) -> Self {
-        let mut parent = self.clone();
-        let new_len = parent.len().saturating_sub(1);
-        parent.truncate(new_len);
-        parent
-    }
-
-    fn deepest(&self) -> usize {
-        self[self.len().saturating_sub(1)]
-    }
-
-    fn with_child(&self, i: usize) -> Self {
-        let mut path = self.clone();
-        path.push(i);
-        path
-    }
-}
-
-impl Deref for TreeNodePath {
-    type Target = Vec<usize>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for TreeNodePath {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl Ord for TreeNodePath {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let mut j = 0;
-        loop {
-            if j >= self.0.len().min(other.0.len()) {
-                break self.0.len().cmp(&other.0.len());
-            }
-
-            let ord = self.0[j].cmp(&other.0[j]);
-
-            if ord != Ordering::Equal {
-                break ord;
-            }
-
-            j += 1;
-        }
-    }
-}
-
-impl Display for TreeNodePath {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for i in &self.0 {
-            write!(f, "/{i}")?;
-        }
-        Ok(())
-    }
-}
+use super::TreeNodePath;
 
 fn cmp_vec(vec_a: &[usize], vec_b: &[usize]) -> Ordering {
     let vec_a = TreeNodePath(vec_a.to_vec());
