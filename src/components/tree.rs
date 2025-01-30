65d7c5cf9f1a060202df62dcb25b39fc8dc63a44
taro <taro@taro.codes>
2025-01-30 20:16:23 -0300

    refactor(Tree): tree_node.rs

--- src/components/tree.rs
+++ src/components/tree.rs
@@ -2,6 +2,8 @@ mod component;
 mod keyboard_handler;
 mod widget;
 mod tree_node_path;
+mod tree_node;
 
-pub use component::{Tree, TreeNode};
+pub use component::{Tree};
+pub use tree_node::*;
 pub use tree_node_path::{TreeNodePath};
---
+++ src/components/tree/tree_node.rs
@@ -0,0 +1,105 @@
+use std::cmp::Ordering;
+use std::collections::VecDeque;
+
+use super::TreeNodePath;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TreeNode<T> {
+    pub inner: T,
+    pub is_visible: bool,
+    pub is_match: bool,
+    pub is_open: bool,
+    pub children: Vec<Self>,
+}
+
+impl<T> TreeNode<T> {
+    pub fn new(t: T) -> Self {
+        Self {
+            inner: t,
+            is_visible: true,
+            is_match: false,
+            is_open: true,
+            children: vec![],
+        }
+    }
+
+    fn total_open_children_count(&self) -> usize {
+        fn recursive_total_open_count<T>(nodes: &[TreeNode<T>]) -> usize {
+            let mut count = 0;
+            for i in 0..nodes.len() {
+                count += 1;
+
+                if !nodes[i].is_open || nodes[i].children.is_empty() {
+                    continue
+                }
+
+                count += recursive_total_open_count(&nodes[i].children);
+            }
+            count
+        }
+        if !self.is_open || self.children.is_empty() {
+            0
+        } else {
+            recursive_total_open_count(&self.children)
+        }
+    }
+
+    pub(crate) fn total_open_count(nodes: &[TreeNode<T>]) -> usize {
+        nodes.len() + nodes.iter().map(|node| node.total_open_children_count()).sum::<usize>()
+    }
+
+    pub(crate) fn open_count(nodes: &[TreeNode<T>], until_path: &TreeNodePath) -> usize {
+        fn recursive_open_count<T>(nodes: &[TreeNode<T>], path: TreeNodePath, until_path: &TreeNodePath) -> usize {
+            let mut count = 0;
+            for i in 0..nodes.len() {
+                let mut new_path = path.clone();
+                new_path.push(i);
+
+                if new_path.cmp(until_path) >= Ordering::Equal {
+                    break;
+                }
+
+                count += 1;
+
+                if !nodes[i].is_open || nodes[i].children.is_empty() {
+                    continue
+                }
+
+                count += recursive_open_count(&nodes[i].children, new_path, &until_path);
+            }
+            count
+        }
+        recursive_open_count(&*nodes, TreeNodePath::empty(), until_path)
+    }
+}
+
+pub fn get_node_at_path<T>(mut path: TreeNodePath, nodes: &[TreeNode<T>]) -> &TreeNode<T> {
+    fn recursive<T>(mut path: VecDeque<usize>, nodes: &[TreeNode<T>]) -> &TreeNode<T> {
+        let Some(next_level) = path.pop_front() else {
+            panic!("get_node_at_path panic");
+        };
+
+        if path.is_empty() {
+            &nodes[next_level]
+        } else {
+            recursive(path, &nodes[next_level].children)
+        }
+    }
+    let path: VecDeque<usize> = path.0.clone().into();
+    recursive(path, nodes)
+}
+
+pub fn get_node_at_path_mut<T>(mut path: TreeNodePath, nodes: &mut[TreeNode<T>]) -> &mut TreeNode<T> {
+    fn recursive<T>(mut path: VecDeque<usize>, nodes: &mut[TreeNode<T>]) -> &mut TreeNode<T> {
+        let Some(next_level) = path.pop_front() else {
+            panic!("get_node_at_path_mut panic");
+        };
+
+        if path.is_empty() {
+            &mut nodes[next_level]
+        } else {
+            recursive(path, &mut nodes[next_level].children)
+        }
+    }
+    recursive(path.0.into(), nodes)
+}
