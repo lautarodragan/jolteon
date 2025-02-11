3ecc69254be7bc120e61e74563dd48a67b77efec
taro <taro@taro.codes>
2025-02-11 02:32:20 -0300

    refactor(TreeNode): TreeNode.iter()

--- src/components/tree/tree_node.rs
+++ src/components/tree/tree_node.rs
@@ -125,4 +125,136 @@ impl<T> TreeNode<T> {
         }
         recursive(path.to_vec().into(), nodes)
     }
+
+    pub fn iter(&self) -> TreeNodeIterator<T> {
+        let iter = TreeNodeIterator {
+            root: &self,
+            current_node: None,
+            current_path: TreeNodePath::empty(),
+        };
+
+        iter
+    }
+}
+
+struct TreeNodeIterator<'a, T> {
+    root: &'a TreeNode<T>,
+    current_node: Option<&'a TreeNode<T>>,
+    current_path: TreeNodePath,
+}
+
+impl<'a, T> Iterator for TreeNodeIterator<'a, T> {
+    // type Item = (&'a TreeNode<T>, &'a TreeNodePath); // enumerate?
+    type Item = &'a TreeNode<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // TODO: avoid calling .get_child(). store branch: Vec<&'a TreeNode<T>> (or VecDeque?)
+
+        if let Some(current_node) = self.current_node {
+            let current_node_first_child = current_node.children().first();
+
+            if current_node_first_child.is_some() {
+                self.current_path = self.current_path.with_child(0);
+                self.current_node = current_node_first_child;
+                current_node_first_child
+            } else {
+                // current_node has no children. move to next sibling, if there are more;
+                // otherwise, move up, if we are not the root;
+                // otherwise, we're done.
+
+                if self.current_path.len() == 1 {
+                    // no more parents. we're at the top level.
+                    // we return the next sibling if there is one, None otherwise.
+                    self.current_path = self.current_path.next();
+                    self.current_node = self.root.children.get(self.current_path.last());
+                    self.current_node
+                } else {
+                    // get next sibling
+                    let parent_path = self.current_path.parent();
+                    let parent = self.root.get_child(&parent_path).unwrap();
+                    let next_sibling_path = self.current_path.next();
+                    let next_sibling = parent.children.get(next_sibling_path.last());
+
+                    if next_sibling.is_some() {
+                        self.current_path = next_sibling_path;
+                        self.current_node = next_sibling;
+                        self.current_node
+                    } else {
+                        // go up. may need to go many levels!
+
+                        self.current_path = self.current_path.parent().next();
+                        self.current_node = self.root.get_child(&self.current_path);
+
+                        self.current_node
+                    }
+                }
+            }
+        } else {
+            if self.root.children.is_empty() {
+                None
+            } else {
+                self.current_path = self.current_path.with_child(0);
+                self.current_node = Some(&self.root.children[0]);
+                self.current_node
+            }
+
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::components::TreeNode;
+
+    #[test]
+    pub fn test_1() {
+
+        let mut child_2 = TreeNode::new("child 2");
+        child_2.children = vec![
+            TreeNode::new("child 2 - 1"),
+            TreeNode::new("child 2 - 2"),
+        ];
+
+        let mut child_3_2 = TreeNode::new("child 3 - 2");
+        child_3_2.children = vec![
+            TreeNode::new("child 3 - 2 - 1"),
+            TreeNode::new("child 3 - 2 - 2"),
+        ];
+
+        let mut child_3 = TreeNode::new("child 3");
+        child_3.children = vec![
+            TreeNode::new("child 3 - 1"),
+            child_3_2,
+            TreeNode::new("child 3 - 3"),
+        ];
+
+        let mut child_4 = TreeNode::new("child 4");
+        child_4.children = vec![
+            TreeNode::new("child 4 - 1"),
+        ];
+
+        let mut root = TreeNode::new("root");
+        root.children = vec![
+            TreeNode::new("child 1"),
+            child_2,
+            child_3,
+            child_4,
+        ];
+
+        let mut iter = root.iter();
+
+        assert!(iter.next().is_some_and(|n| n.inner == "child 1"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 2"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 2 - 1"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 2 - 2"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 3"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 3 - 1"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 3 - 2"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 3 - 2 - 1"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 3 - 2 - 2"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 3 - 3"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 4"));
+        assert!(iter.next().is_some_and(|n| n.inner == "child 4 - 1"));
+        assert!(iter.next().is_none());
+    }
 }
