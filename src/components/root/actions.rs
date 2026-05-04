697ce29e68cdb338722162231711ce0eb583a3ff
taro <taro@taro.codes>
2026-05-03 21:24:20 -0300

    refactor: handle actions (plural) in root/actions.rs

--- src/components/root/actions.rs
+++ src/components/root/actions.rs
@@ -5,70 +5,77 @@ use crate::{
 };
 
 impl OnActionMut for Root<'_> {
-    fn on_action(&mut self, action: Vec<Action>) {
+    fn on_action(&mut self, actions: Vec<Action>) {
         if self.query.borrow().is_some() {
             let mut query_error = self.query_error.borrow_mut();
             if query_error.is_some() {
-                match action[0] {
-                    Action::Confirm | Action::Cancel => {
-                        *query_error = None;
+                for action in actions {
+                    match action {
+                        Action::Confirm | Action::Cancel => {
+                            *query_error = None;
+                            return;
+                        }
+                        _ => {}
                     }
-                    _ => {}
                 }
             } else {
-                match action[0] {
-                    Action::Cancel => {
-                        *self.query.borrow_mut() = None;
-                    }
-                    Action::Confirm => {
-                        let query = self.query.borrow_mut().take();
-                        match query {
-                            None => {}
-                            Some(Query::AddSongs { songs, target }) => match target {
-                                QueryAddSongsArg1::Library => {
-                                    self.library_screen.borrow_mut().add_songs(songs);
-                                }
-                                QueryAddSongsArg1::Soundtracks => {
-                                    let (songs_soundtracks, songs_etc): (Vec<_>, Vec<_>) =
-                                        songs.into_iter().partition(|song| song.soundtrack_subject.is_some());
+                for action in actions {
+                    match action {
+                        Action::Cancel => {
+                            *self.query.borrow_mut() = None;
+                            return;
+                        }
+                        Action::Confirm => {
+                            let query = self.query.borrow_mut().take();
+                            match query {
+                                None => {}
+                                Some(Query::AddSongs { songs, target }) => match target {
+                                    QueryAddSongsArg1::Library => {
+                                        self.library_screen.borrow_mut().add_songs(songs);
+                                    }
+                                    QueryAddSongsArg1::Soundtracks => {
+                                        let (songs_soundtracks, songs_etc): (Vec<_>, Vec<_>) =
+                                            songs.into_iter().partition(|song| song.soundtrack_subject.is_some());
 
-                                    if !songs_soundtracks.is_empty() {
-                                        self.soundtracks_screen.borrow_mut().add_songs(songs_soundtracks);
+                                        if !songs_soundtracks.is_empty() {
+                                            self.soundtracks_screen.borrow_mut().add_songs(songs_soundtracks);
+                                        }
+                                        if !songs_etc.is_empty() {
+                                            *query_error = Some(format!(
+                                                "{} song(s) could not be added to soundtracks!",
+                                                songs_etc.len()
+                                            ));
+                                            *self.query.borrow_mut() = Some(Query::AddSongs {
+                                                songs: songs_etc,
+                                                target: QueryAddSongsArg1::Soundtracks,
+                                            });
+                                        }
                                     }
-                                    if !songs_etc.is_empty() {
-                                        *query_error = Some(format!(
-                                            "{} song(s) could not be added to soundtracks!",
-                                            songs_etc.len()
-                                        ));
-                                        *self.query.borrow_mut() = Some(Query::AddSongs {
-                                            songs: songs_etc,
-                                            target: QueryAddSongsArg1::Soundtracks,
-                                        });
+                                    QueryAddSongsArg1::Playlist => {
+                                        self.playlists_screen.borrow_mut().add_songs(songs);
                                     }
-                                }
-                                QueryAddSongsArg1::Playlist => {
-                                    self.playlists_screen.borrow_mut().add_songs(songs);
-                                }
-                            },
+                                },
+                            }
+                            return;
                         }
-                    }
-                    Action::Navigation(NavigationAction::Right) => {
-                        if let Some(Query::AddSongs { target, .. }) = self.query.borrow_mut().as_mut() {
-                            *target = target.next();
+                        Action::Navigation(NavigationAction::Right) => {
+                            if let Some(Query::AddSongs { target, .. }) = self.query.borrow_mut().as_mut() {
+                                *target = target.next();
+                            }
+                            return;
                         }
-                    }
-                    Action::Navigation(NavigationAction::Left) => {
-                        if let Some(Query::AddSongs { target, .. }) = self.query.borrow_mut().as_mut() {
-                            *target = target.prev();
+                        Action::Navigation(NavigationAction::Left) => {
+                            if let Some(Query::AddSongs { target, .. }) = self.query.borrow_mut().as_mut() {
+                                *target = target.prev();
+                            }
+                            return;
                         }
-                    }
-                    _ => {
-                        // TODO: do something with the query
+                        _ => {}
                     }
                 }
             }
         } else {
-            match action[0] {
+            match actions[0] {
                 Action::Screen(action) if !self.is_focus_trapped.get() => match action {
                     ScreenAction::Next => {
                         if self.focused_screen < 5 {
@@ -93,7 +100,7 @@ impl OnActionMut for Root<'_> {
                 },
                 _ => {
                     let mut c = self.screens[self.focused_screen].1.borrow_mut();
-                    c.on_action(action);
+                    c.on_action(actions);
                 }
             }
         }
